1e71aa3aa89424a7af0f3582711b530f54b6ca54
NatalyaKhrol <tashapas@yahoo.com>
2022-08-21 20:16:02 +0300

    Home Task is done

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -13,16 +13,5 @@ public class ProductTest extends BaseTest {
         driver.findElement(By.id("shopping_cart_container")).click();
         driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']//ancestor::div[@class='cart_item']//div[@class='inventory_item_price']")).getText();
 
-        /*
-<button class="btn btn_primary btn_small btn_inventory"
-    data-test="add-to-cart-sauce-labs-backpack"
-    id="add-to-cart-sauce-labs-backpack"
-    name="add-to-cart-sauce-labs-backpack">Add to cart</button>
-        //button[text()='Add to cart']
-        //div[text()='Sauce Labs Backpack']//ancestor::div[@class='inventory_item']//button
-        //div[text()='Sauce Labs Backpack']//ancestor::div[@class='cart_item']//div[@class='inventory_item_price']
-         */
-
-
     }
 }
